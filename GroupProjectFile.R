05bb5268e163e0e504ef090ab9db976b244fa242
keconlon <42517994+keconlon@users.noreply.github.com>
2018-10-26 11:26:18 -0500

    Add files via upload

--- GroupProjectFile.R
+++ GroupProjectFile.R
@@ -1,14 +1,29 @@
 rm(list=ls())
 
+# Read in Iowa Liquor Sales data and subset into separate 2016 & 2017 CSV files.
+
 df <- read.csv("Iowa_Liquor_Sales.csv")
-
-df$Invoice.Item.Number <- as.numeric(df$Invoice.Item.Number)
 df$Date <- as.Date(df$Date, "%m/%d/%Y")
-df$Address <- as.character(df$Address)
-df$Store.Location <- as.character(df$Store.Location)
-df$Item.Number <- as.character(df$Item.Number)
-df$Item.Description <- as.character(df$Item.Description)
-df$Year <- as.Date(df$Year, "%m/%d/%Y")
-df_test <- subset(df, Date >= "2015-01-01" & Date < "2018-01-01")
 
-write.csv(df_test, "Iowa_Liquor_Sales_2015-2017.csv", row.names = FALSE)
+df_test <- subset(df, Date >= "2016-01-01" & Date < "2017-01-01")
+write.csv(df_test, "Iowa_Liquor_Sales_2016.csv", row.names = FALSE)
+
+NA_count_2016 <- sum(is.na(df_test))
+
+df_test <- subset(df, Date >= "2017-01-01" & Date < "2018-01-01")
+write.csv(df_test, "Iowa_Liquor_Sales_2017.csv", row.names = FALSE)
+
+NA_count_2017 <- sum(is.na(df_test))
+
+# Clear previous work, read in the data set(s) of interest, and clean them up.
+
+rm(list=ls())
+df_sales <- read.csv("Iowa_Liquor_Sales_2017.csv", na.strings = c("NA", ""))
+
+df_sales$Invoice.Item.Number <- as.numeric(df_sales$Invoice.Item.Number)
+df_sales$Date <- as.Date(df_sales$Date, "%m/%d/%Y")
+df_sales$Address <- as.character(df_sales$Address)
+df_sales$Store.Location <- as.character(df_sales$Store.Location)
+df_sales$Item.Number <- as.character(df_sales$Item.Number)
+df_sales$Item.Description <- as.character(df_sales$Item.Description)
+
