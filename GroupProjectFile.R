70fdbface7fa268b3b176071d1c018fc9575ee66
Hunter Somers <42579246+hsomers@users.noreply.github.com>
2018-11-20 14:28:49 -0600

    Add files via upload
    I added the top vendor by county analysis to the end of the "analysis" section. Note: I used the library, sqldf, to do this and I've added the library(sqldf) command at the top.

--- GroupProjectFile.R
+++ GroupProjectFile.R
@@ -14,22 +14,23 @@ library(reshape2)
 library(scales)
 suppressPackageStartupMessages(library(choroplethr))
 library(choroplethrMaps)
+library(sqldf)
 
 ##############################################################################
 #                           DATA CLEANSING
 ##############################################################################
 
 # LOAD ALL DATA FRAMES
-  #Liquor sales df
+#Liquor sales df
 df_sales <- read.csv("Iowa_Liquor_Sales_2017.csv", na.strings = c("NA", ""))
 
-  #Census df
+#Census df
 df_census <- read.csv("County_Population_in_Iowa_by_Year.csv")
 
 # LOAD & CLEANSE COUNTY.REGIONS DF
-  # Load detailed county info
+# Load detailed county info
 data(county.regions)
-  # Subset to just Iowa
+# Subset to just Iowa
 county.regions <- subset(county.regions, state.abb == "IA")
 county.regions <- county.regions[, c("region", "county.name")]
 
@@ -43,46 +44,46 @@ df_sales$County.Number <- factor(df_sales$County.Number)
 df_sales$County <- factor(df_sales$County)
 df_sales$Category <- factor(df_sales$Category)
 df_sales$Vendor.Number <- factor(df_sales$Vendor.Number)
-  #Kept this...sometimes Item.Description is the same but Item.Number is different for two data points
+#Kept this...sometimes Item.Description is the same but Item.Number is different for two data points
 df_sales$Item.Number <- factor(df_sales$Item.Number)
-  #Clean up county spellings
-      #Add levels
+#Clean up county spellings
+#Add levels
 levels(df_sales$County) <- c(levels(df_sales$County), "buena vista", "cerro gordo", "o'brien", "pottawattamie")
-      #Overwrite levels with accurate names
+#Overwrite levels with accurate names
 df_sales$County[11] <- "buena vista"
 df_sales$County[17] <- "cerro gordo"
 df_sales$County[71] <- "o'brien"
 df_sales$County[78] <- "pottawattamie"
-      #Remove inaccurate level names
+#Remove inaccurate level names
 levels(df_sales$County)[levels(df_sales$County) == "buena vist"] <- "buena vista"
 levels(df_sales$County)[levels(df_sales$County) == "cerro gord"] <- "cerro gordo"
 levels(df_sales$County)[levels(df_sales$County) == "obrien"] <- "o'brien"
 levels(df_sales$County)[levels(df_sales$County) == "pottawatta"] <- "pottawattamie"
-  # Pull in FIPS code
-      # Rename county.regions df for merge-specific cleansing
+# Pull in FIPS code
+# Rename county.regions df for merge-specific cleansing
 fips <- county.regions
-      # Rename column: county.name to County
+# Rename column: county.name to County
 names(fips)[names(fips) == "county.name"] <- "County"
 names(fips)[names(fips) == "region"] <- "FIPS"
-      # Merge FIPS column into df_sales
+# Merge FIPS column into df_sales
 df_sales <- merge(df_sales, fips, all.x = TRUE)
 
 
 # CLEANSE CENSUS DF
-  #column not needed; delete
+#column not needed; delete
 df_census$Primary.County.Coordinates <- NULL 
 df_census$FIPS <- factor(df_census$FIPS)
-  #Note: left Date as a factor on purpose.
+#Note: left Date as a factor on purpose.
 
-  #Examine Population Data for July 01, 2017 to ensure the 
-  # "July 01 2017" Census includes data for all 99 counties.
+#Examine Population Data for July 01, 2017 to ensure the 
+# "July 01 2017" Census includes data for all 99 counties.
 pop_est_2017 <- subset(df_census, Year == "July 01 2017")
 print(nrow(pop_est_2017))
 
-  #Subset to just 2017
+#Subset to just 2017
 df_census <- subset(df_census, Year == "July 01 2017")
 
-  #Add the Urban/Rural Column
+#Add the Urban/Rural Column
 df_census$rural_or_urban <- df_census$Population
 option_urban <- which(df_census$rural_or_urban >= 50000)
 option_rural <- which(df_census$rural_or_urban <= 49999)
@@ -122,24 +123,37 @@ descriptions <- analyze_by(df_sales, quo(Item.Description))
 cities <- analyze_by(df_sales, quo(City))
 
 # WHICH COUNTIES BUY THE MOST ALCOHOL (BY VOLUME LITERS/PER CAPITA)?
-  #Summarize volume by county
+#Summarize volume by county
 county <- analyze_by(df_sales, quo(FIPS))
-  #Merge summary with census df
+#Merge summary with census df
 county_pc <- merge(county, df_census, all.x = TRUE)
-  #Calculate per capita consumption
+#Calculate per capita consumption
 county_pc$Vol_Per_Cap <- round(county_pc$VolSold / county_pc$Population, 1)
-  #Remove irrelevant columns
+#Remove irrelevant columns
 county_pc$VolSold <- NULL
 county_pc$Year <- NULL
 county_pc$Population <- NULL
 county_pc$rural_or_urban <- NULL
-  #Order in descending order (highest to lowest consumption)
+#Order in descending order (highest to lowest consumption)
 county_pc <- arrange(county_pc, desc(Vol_Per_Cap))
 
 # VOLUME SOLD BY WEEK
 
 df_sales$date_binned <- cut(df_sales$Date, breaks = "weeks")
 Date <- analyze_by(df_sales, quo(date_binned))
+
+# TOP VENDOR FOR EACH COUNTY
+vendor_by_county <- df_sales %>% # select the columns we want
+  select(County, Vendor.Name, Volume.Sold..Liters.)
+vendor_by_county$County <- tolower(vendor_by_county$County) # make case-insensitive
+vendor_by_county <- vendor_by_county %>%
+  group_by(County, Vendor.Name) %>% # group by county and vendor name
+  summarize(VolSold = sum(Volume.Sold..Liters.)) # sum based on county & vendor name combo
+
+colnames(vendor_by_county)[colnames(vendor_by_county)=="Vendor.Name"] <- "VendorName" #Rename column to work in SQL command
+
+vendor_by_county <- sqldf("select County, VendorName, max(VolSold) from vendor_by_county where County != '' group by County") # Find the biggest supplier for each county
+
 
 ##############################################################################
 #                           VISUALIZATIONS
